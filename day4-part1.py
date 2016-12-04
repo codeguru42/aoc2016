27375a1b64d6a278c48018910bd96bf9da0504d8
Code Apprentice <codeguru42@gmail.com>
2016-12-04 10:04:16 -0700

    Day 4 Part 1: Try to implement isRealRoom()

--- day4-part1.py
+++ day4-part1.py
@@ -1,6 +1,7 @@
 import unittest
 import re
 import string
+import operator
 
 class Day4(unittest.TestCase):
   def test1(self):
@@ -31,9 +32,12 @@ def isRealRoom(room):
   m = re.fullmatch(roomRegEx, room)
   if m:
     c = alphaCount(m.group(0))
-    print(c)
-  else:
-    print("Invalid room:", repr(room))
+    sorted_c = sorted(c.items(), key=operator.itemgetter(1), reverse=True)
+    letters = [pair[0] for pair in sorted_c[:5]]
+    print(letters)
+    print(m.groups())
+    return ''.join(letters) == m.group(2)
+  return False
 
 if __name__ == "__main__":
   unittest.main()
