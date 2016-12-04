dbbfa5b1272b1efc7c0b795e59e46bbbb6fdd456
Code Apprentice <codeguru42@gmail.com>
2016-12-03 23:11:36 -0700

    Day 4 Part 1: alphacount()

--- day4-part1.py
+++ day4-part1.py
@@ -1,4 +1,14 @@
 import unittest
 
+def alphaCount(text):
+  counts = dict()
+  for c in string.ascii_lowercase:
+    counts[c] = 0
+
+  for c in text:
+    counts[c] += 1
+
+  return counts
+
 if __name__ == "__main__":
   unittest.main()
