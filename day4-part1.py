6d354f125a2ff77c721a2d087eb0b4ecc5666c07
Code Apprentice <codeguru42@gmail.com>
2016-12-03 23:21:52 -0700

    Day 4 Part 1: Write unit tests

--- day4-part1.py
+++ day4-part1.py
@@ -1,5 +1,18 @@
 import unittest
 
+class Day4(unittest.TestCase):
+  def test1(self):
+    self.assertTrue(isRealRoom("aaaaa-bbb-z-y-x-123[abxyz]"))
+
+  def test2(self):
+    self.assertTrue(isRealRoom("a-b-c-d-e-f-g-h-987[abcde]"))
+
+  def test3(self):
+    self.assertTrue(isRealRoom("not-a-real-room-404[oarel]"))
+
+  def test4(self):
+    self.assertFalse(isRealRoom("totally-real-room-200[decoy]"))
+
 def alphaCount(text):
   counts = dict()
   for c in string.ascii_lowercase:
