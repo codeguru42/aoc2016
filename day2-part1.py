6d42d0c34c09dbaf4592d25536c4fdfe4b893eae
Code Apprentice <codeguru42@gmail.com>
2016-12-04 22:05:08 -0700

    Day 2 Part 1: Implement getBathroomCode()

--- day2-part1.py
+++ day2-part1.py
@@ -7,9 +7,17 @@ class Day2(unittest.TestCase):
 
 def getBathroomCode(instructions):
   moves = {'U':-3, 'D':+3, 'L':-1, 'R':+1}
+  code = []
+  pos = 5
 
   for line in instructions.split('\n'):
-    print(line)
+    for c in line:
+      pos += moves[c]
+      if pos < 1 or pos > 9:
+        pos -= moves[c]
+    code.append(pos)
+
+  return ''.join(map(str, code))
 
 def main():
   pass
