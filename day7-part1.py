8ed5b27a4e83946b914c9a7913e8ff75c50ca8bb
Code Apprentice <codeguru42@gmail.com>
2016-12-07 19:29:48 -0700

    Day 7 Part 1: Implement tests

--- day7-part1.py
+++ day7-part1.py
@@ -2,7 +2,20 @@ import unittest
 
 class Day7Part1(unittest.TestCase):
   def test1(self):
-    pass
+    ip = "abba[mnop]qrst"
+    self.assertTrue(supportsTLS(ip))
+
+  def test2(self):
+    ip = "abcd[bddb]xyyx"
+    self.assertTrue(supportsTLS(ip))
+
+  def test3(self):
+    ip = "aaaa[qwer]tyui"
+    self.assertFalse(supportsTLS(ip))
+
+  def test4(self):
+    ip = "ioxxoj[asdfgh]zxcvbn"
+    self.assertTrue(supportsTLS(ip))
 
 def main():
   pass
