144123f5fc4dd73a9b31485fc3af1f6ba70d8732
Code Apprentice <codeguru42@gmail.com>
2016-12-16 11:44:25 -0700

    Day 3 Part 2: Attempted solution

--- day03.py
+++ day03.py
@@ -3,9 +3,20 @@ import sys
 def is_triangle(a, b, c):
   return a + b > c and a + c > b and b + c > a
 
+lines = sys.stdin.readlines()
 count = 0
-for line in sys.stdin:
+for line in lines:
   data = line.split()
   if is_triangle(int(data[0]), int(data[1]), int(data[2])):
     count += 1
+print(count)
+
+count = 0
+for group in zip(*[iter(lines)] * 3):
+  triangles = list(map(str.split, group))
+  print("triangles:", triangles)
+  for i in range(3):
+    print(triangles[0][i], triangles[1][i], triangles[2][i])
+    if is_triangle(triangles[0][i], triangles[1][i], triangles[2][i]):
+      count += 1
 print(count)
