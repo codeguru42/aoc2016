5c6e892bc9a37939cc4cea67711df2ca8cfe3f71
Code Apprentice <codeguru42@gmail.com>
2016-12-16 04:36:53 -0700

    Day 16 Part 2: ChecksumTest

--- day16.py
+++ day16.py
@@ -21,5 +21,16 @@ class DragonTest(unittest.TestCase):
     expected = "1111000010100101011110000"
     self.assertEqual(expected, dragon(binary))
 
+class ChecksumTest(unittest.TestCase):
+  def test1(self):
+    data = "110010110100"
+    expected = "100"
+    self.assertEqual(expected, checksum(data))
+
+  def test2(self):
+    data = "10000011110010000111"
+    expected = "01100"
+    self.assertEqual(expected, checksum(data))
+
 if __name__ == "__main__":
   unittest.main()
