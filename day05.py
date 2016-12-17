9d157dc571469d544008d6e613022cf907796f2f
Code Apprentice <codeguru42@gmail.com>
2016-12-17 14:09:16 -0700

    Day 5 Part 1: Write tests for MD5

--- day05.py
+++ day05.py
@@ -1,4 +1,27 @@
 import unittest
+import hashlib
+
+class MD5Test(unittest.TestCase):
+  def setUp(self):
+    self.m = hashlib.md5()
+
+  def test1(self):
+    data = 'abc3231929'.encode('utf-8')
+    self.m.update(data)
+    result = self.m.hexdigest()
+    self.assertEqual('00000', result[:5])
+
+  def test2(self):
+    data = 'abc5017308'.encode('utf-8')
+    self.m.update(data)
+    result = self.m.hexdigest()
+    self.assertEqual('00000', result[:5])
+
+  def test3(self):
+    data = 'abc5278568'.encode('utf-8')
+    self.m.update(data)
+    result = self.m.hexdigest()
+    self.assertEqual('00000', result[:5])
 
 if __name__ == "__main__":
   unittest.main()
