77c69e66f67374ab1ac5053fb84caa6c7c7066fc
Code Apprentice <codeguru42@gmail.com>
2016-12-18 11:52:31 -0700

    Day 8 Part 1: First unit test

--- day08.py
+++ day08.py
@@ -1,5 +1,14 @@
 import unittest
 
+class ScreenTest(unittest.TestCase):
+  def setUp(self):
+    self.screen = screen(7, 3)
+
+  def testRect(self):
+    self.screen.rect(3, 2)
+    expected = '###....\n###....\n.......'
+    self.assertEquals(expected, str(screen))
+
 if __name__ == "__main__":
   if unittest.main(exit=False).result.wasSuccessful():
     pass
