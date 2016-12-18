4355f84d12c81023e61bc2742f2d03ef2670c59a
Code Apprentice <codeguru42@gmail.com>
2016-12-17 19:31:51 -0700

    Day 6 Part 1: Implement test from example

--- day06.py
+++ day06.py
@@ -1,4 +1,25 @@
 import unittest
 
+class Day6Part1Test(unittest.TestCase):
+  def test1(self):
+    data = '''eedadn
+              drvtee
+              eandsr
+              raavrd
+              atevrs
+              tsrnev
+              sdttsa
+              rasrtv
+              nssdts
+              ntnada
+              svetve
+              tesnvt
+              vntsnd
+              vrdear
+              dvrsen
+              enarar'''
+    message = 'easter'
+    self.assertEqual(message, get_message(data))
+
 if __name__ == '__main__':
   unittest.main()
