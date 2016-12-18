9f1baf7bed19a753628b39b75e2e3081ab99399c
Code Apprentice <codeguru42@gmail.com>
2016-12-18 12:17:20 -0700

    Day 8 Part 1: Initial version of screen class

--- day08.py
+++ day08.py
@@ -7,7 +7,21 @@ class ScreenTest(unittest.TestCase):
   def testRect(self):
     self.screen.rect(3, 2)
     expected = '###....\n###....\n.......'
-    self.assertEquals(expected, str(screen))
+    self.assertEquals(expected, str(self.screen))
+
+class screen:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    self.screen = [['.'] * width for _ in range(height)]
+
+  def __str__(self):
+    return '\n'.join([''.join(c) for c in self.screen])
+
+  def rect(self, width, height):
+    for i in range(width):
+      for j in range(height):
+        self.screen[j][i] = '#'
 
 if __name__ == "__main__":
   if unittest.main(exit=False).result.wasSuccessful():
