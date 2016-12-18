3adbdb2e2a950aedeaa1b3b7d9444d45e33af88b
Code Apprentice <codeguru42@gmail.com>
2016-12-17 22:47:16 -0700

    Day 18 Part 1: Add test case

--- day18.py
+++ day18.py
@@ -1,5 +1,11 @@
 import unittest
 
+class NextRowTest(unittest.TestCase):
+  def test1(self):
+    row = '.^^^^'
+    next_row = '^^..^'
+    self.assertEqual(next_row, get_next_row(row))
+
 if __name__ == "__main__":
   if unittest.main(exit=False).result.wasSuccessful():
     pass
