112c7fcc382e8bbcbf7668c00df973f77cc4b06c
Code Apprentice <codeguru42@gmail.com>
2016-12-17 20:04:31 -0700

    Day 6 Part 1: Implement get_message()

--- day06.py
+++ day06.py
@@ -1,4 +1,5 @@
 import unittest
+import io
 
 class Day6Part1Test(unittest.TestCase):
   def test1(self):
@@ -19,10 +20,26 @@ class Day6Part1Test(unittest.TestCase):
               dvrsen
               enarar'''
     message = 'easter'
-    self.assertEqual(message, get_message(data))
+    self.assertEqual(message, get_message(io.StringIO(data)))
 
 def get_message(data):
-  pass
+  counts = None
+  for line in data:
+    line = line.strip()
+    if counts == None:
+      counts = [{} for _ in range(len(line))]
+    for i, c in enumerate(line):
+      counts[i][c] = counts[i].get(c, 0) + 1
+
+  message = []
+  for d in counts:
+    maxCount = 0
+    for key in d:
+      if d[key] > maxCount:
+        maxCount = d[key]
+        c = key
+    message.append(c)
+  return ''.join(message)
 
 if __name__ == '__main__':
   unittest.main()
